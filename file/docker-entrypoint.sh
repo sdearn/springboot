0ffe2bc2ab1b142dac87a9a3d6c8665038836ce8
sdearn <31753221+sdearn@users.noreply.github.com>
2019-12-20 15:49:45 +0800

    Update docker-entrypoint.sh

--- file/docker-entrypoint.sh
+++ file/docker-entrypoint.sh
@@ -2,11 +2,4 @@
 
 set -x
 
-app_name="${APP_NAME:-default}"
-build_cmd="${BUILD_CMD:-clean package -Dmaven.test.skip=true}"
-environment="${ENVIRONMENT:-pom.xml.test}"
-
-cd ../../
-
-sed -i "2i JAVA_OPTS=\"$JAVA_OPTS -Dfile.encoding=UTF8  -Duser.timezone=GMT+08\"" /work/tomcat/bin/catalina.sh
 
